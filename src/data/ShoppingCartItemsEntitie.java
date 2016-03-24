5bdc29aba054393e99965d3135f8c1ca3f368a64
Ryan Ardiente <Nayrdnt@Ryans-MacBook-Pro.local>
2016-03-24 13:34:49 -0600

    fixed the shopping cart issues

--- src/data/ShoppingCartItemsEntitie.java
+++ src/data/ShoppingCartItemsEntitie.java
@@ -7,6 +7,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
@@ -16,6 +17,7 @@ import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 @Table(name = "ShoppingcartItems")
+@NamedQuery(name = "getShoppingCartItem", query = "select s from ShoppingCartItemsEntitie s where s.shoppingCart_id = :sid and s.products_id = :pid")
 public class ShoppingCartItemsEntitie {
 
 	@Id
